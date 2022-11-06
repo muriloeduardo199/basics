918e2b00c11cb11e77578012fb2539e46f35a77f
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-11-06 09:43:33 -0300

    update

--- intermediary/exer_funcoes_1_2.py
+++ intermediary/exer_funcoes_1_2.py
@@ -2,12 +2,11 @@
 1 - Crie uma função que exibe uma saudação com os parâmetros saudacao e nome.
 """
 
+
 # def saudacao(saudacao, nome):
 #     print(f'{saudacao} {nome}')
-#
-# saudacao('Olá', 'Joãozinho')
-# saudacao('Oi', 'Maria')
-# saudacao('Hey', 'Eduardo')
+
+# saudacao('ola', 'Murilo')
 
 
 """
@@ -15,54 +14,53 @@
 eles.
 """
 
-# def soma(n1, n2, n3):
-#     print(n1 + n2 + n3)
-#
-#
-# soma(2, 1, 3)
-# soma(1, 1, 1)
-# soma(2, 1, 1)
 
+# def soma(n1,n2,n3):
+#     print(n1+n2+n3)
+# soma(1,2,3)
 """
 3 - Crie uma função que receba 2 números. O primeiro é um valor e o segundo um
 percentual (ex. 10%). Retorne (return) o valor do primeiro número somado
 do aumento do percentual do mesmo.
 """
+def recebendo(valor, percent):
+    return valor*(valor*percent/100)
 
-# def aumento_percentual(valor, percentual):
-#     return valor + (valor * percentual / 100)
-#
-#
-# ap = aumento_percentual(50, 10)
-# print(ap)
-# ap = aumento_percentual(100, 10)
-# print(ap)
-# ap = aumento_percentual(10, 10)
-# print(ap)
-# ap = aumento_percentual(15, 100)
-# print(ap)
-
-
+ex=recebendo(50,10)
+print(ex)
 """
 4 - Fizz Buzz - Se o parâmetro da função for divisível por 3, retorne fizz,
 se o parâmetro da função for divisível por 5, retorne buzz. Se o parâmetro da
 função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o
 número enviado.
 """
-
-
 def fb(n):
-    if n % 3 == 0 and n % 5 == 0:
-        return f'fizzbuzz, {n} é divisível por 3 e 5'
-    if n % 5 == 0:
-        return f'buzz, {n} é divisível por 5'
-    if n % 3 == 0:
-        return f'fizz, {n} é divisível por 3'
+    if n%3==0 and n%5==0:
+        return f'fizzbuzz, {n} e divisivel por 3 e 5'
+    if n%5==0:
+        return f'fizzbuzz, {n} e divisivel  5'
+    if n%3==0:
+        return f'fizzbuzz, {n} e divisivel  3'
     return n
-
 
 from random import randint
 
 for i in range(100):
-    aleatorio = randint(0, 100)
-    print(fb(aleatorio))
+    teste = randint(0,100)
+    print(fb(teste))
+
+# def fb(n):
+#     if n % 3 == 0 and n % 5 == 0:
+#         return f'fizzbuzz, {n} é divisível por 3 e 5'
+#     if n % 5 == 0:
+#         return f'buzz, {n} é divisível por 5'
+#     if n % 3 == 0:
+#         return f'fizz, {n} é divisível por 3'
+#     return n
+
+
+# from random import randint
+
+# for i in range(100):
+#     aleatorio = randint(0, 100)
+#     print(fb(aleatorio))
