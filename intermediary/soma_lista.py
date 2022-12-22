2eacd968eeebb60d09a0ec2d3fdc3e3b99b2b612
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-12-22 10:55:13 -0300

    update

---
+++ intermediary/soma_lista.py
@@ -0,0 +1,9 @@
+from itertools import zip_longest
+
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+
+
+lista_soma = [x+y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
+
+print(lista_soma)
