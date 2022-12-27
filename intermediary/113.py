db6c2f1c48ebca4f4aea397f4c3d83c59a2d049c
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-12-27 09:26:16 -0300

    update

---
+++ intermediary/113.py
@@ -0,0 +1,17 @@
+# reduce - faz a recucao de um iteravel em um  valor
+from itertools import reduce
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+total = 0
+for p in produtos:
+    total +=p['preco']
+    
+
+print(total)
