531a82016dc9600fe93790f477552de4235af661
muriloeduardo199 <muriloeduardo1993@gmail.com>
2023-01-02 18:48:19 -0300

    update

---
+++ intermediary/116.py
@@ -0,0 +1,41 @@
+# Criando arquivos com Python + Context Manager with
+# Usamos a função open para abrir
+# um arquivo em Python (ele pode ou não existir)
+# Modos:
+# r (leitura), w (escrita), x (para criação)
+# a (escreve ao final), b (binário)
+# t (modo texto), + (leitura e escrita)
+# Context manager - with (abre e fecha)
+# Métodos úteis
+# write, read (escrever e ler)
+# writelines (escrever várias linhas)
+# seek (move o cursor)
+# readline (ler linha)
+# readlines (ler linhas)
+# Vamos falar mais sobre o módulo os, mas:
+# os.remove ou unlink - apaga o arquivo
+# os.rename - troca o nome ou move o arquivo
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load
+
+import os
+
+# caminho_arquivo = '/home/murilo/estudos/intermediary'
+caminho_arquivo = 'aula118.txt'
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('linha 1\n')
+    arquivo.write('linha 2\n')
+    print(arquivo.read())
+
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
+    os.remove(caminho_arquivo)
+
+# arquivo = open(caminho_arquivo, 'w')
+# arquivo.close()
